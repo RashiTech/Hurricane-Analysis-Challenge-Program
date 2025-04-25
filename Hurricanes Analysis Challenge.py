0bd9cf3c73de3e6e40b42bf6d1f5f738db4ddbd1
Rashi <rashiai.2022@gmail.com>
2025-04-25 13:21:38 +0530

    added a display statement in the end

--- Hurricanes Analysis Challenge.py
+++ Hurricanes Analysis Challenge.py
@@ -19,7 +19,7 @@ damages = ['Damages not recorded', '100M', 'Damages not recorded', '40M', '27.9M
 # deaths for each hurricane
 deaths = [90,4000,16,3103,179,184,408,682,5,1023,43,319,688,259,37,11,2068,269,318,107,65,19325,51,124,17,1836,125,87,45,133,603,138,3057,74]
 
-# 1
+# 1 #2
 # Update Recorded Damages
 conversion = {"M": 1000000,
               "B": 1000000000}
@@ -194,3 +194,4 @@ def rating_by_damage(hurricanes):
   
 # categorize hurricanes in new dictionary with damage severity as key
 print("\n Hurricanes rated by severity of damage\n", rating_by_damage(hurricane))
+print(" Awesome Job")
